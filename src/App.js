b94ab3b131875765f6255fa621b5d2934e9f3ee0
Burak <mburak.demir.059@gmail.com>
2022-07-31 14:16:23 +0300

    Filter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Item from './components/Item';
 import useAxios from './hooks/useAxios';
 
 function App() {
-
   const {response}=useAxios();
-  console.log(response)
-  return (
+  const [selectone,setSelectOne]=useState("");
+  const [selecttwo,setSelectTwo]=useState("");
+
+  const options=response?.filter_options;
+
+  const filteredResponse=response?.products?.filter(item=>
+      item?.category?.includes(selectone) && item.color.includes(selecttwo)
+  )
+    return (
     <div className="App">
       <Header/>
-      <h1 className='text-4xl'>Items</h1>
+      
+{
+  options &&
+      <div>
+      <select  value={selectone} onChange={(e)=>setSelectOne(e.target.value)}>
+        { 
+          options[0]?.values.map((item,id)=>(
+            <option key={id} value={item}>{item}</option>
+          ))
+        }
+      </select>
+
+      <select  value={selecttwo} onChange={(e)=>setSelectTwo(e.target.value)}>
+        { 
+          options[1]?.values.map((item,id)=>(
+            <option key={id} value={item}>{item}</option>
+          ))
+        }
+      </select>
+      </div>
+}
+
       <div className='flex items-center justify-center flex-wrap space-x-2'>
       {
-        response?.products?.map((item)=>(
+        filteredResponse?.map((item)=>(
           <Item key={item.id} item={item}/>
         ))
       }</div>
 
-      <h1>Toplam Ürün Sayısı:{response?.total_product_count}</h1>
+      <h1 className='text-center text-xl m-4'>Toplam Ürün Sayısı:{response?.total_product_count}</h1>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ const Header = () => {
   return (
     <div className='flex items-center justify-between px-32 py-5 border-b-2 border-black'>
         <p className='text-xl cursor-pointer'>momentup.</p>
-        <ul className='flex space-x-2'>
+        
+        <ul className='flex space-x-5'>
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Collection</a></li>
@@ -12,9 +13,9 @@ const Header = () => {
         </ul>
 
         <div className='flex space-x-2'>
-        <HeartIcon className='w-6 h-6 cursor-pointer '/>
-        <UserIcon className='w-6 h-6 cursor-pointer' />
-        <ShoppingCartIcon className='w-6 h-6 cursor-pointer' />
+            <HeartIcon className='w-6 h-6 cursor-pointer '/>
+            <UserIcon className='w-6 h-6 cursor-pointer' />
+            <ShoppingCartIcon className='w-6 h-6 cursor-pointer' />
         </div>
     </div>
   )
